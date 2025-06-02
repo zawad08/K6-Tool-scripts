549968967d73547cc1f419fcd16f2cf07e065f89
zawad08 <zawadahmed08@gmail.com>
2025-06-02 12:30:47 +0600

    Create Load(10_vus).js

---
+++ Load(10_vus).js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export let options = {
+  vus: 10,            // 50 virtual users
+  duration: '30s',     // test duration of 1 minute
+  thresholds: {
+    http_req_duration: ['p(95)<2000'], // 95% of requests should be < 2s
+    http_req_failed: ['rate<0.01'],    // < 1% requests should fail
+  },
+};
+
+export default function () {
+  const res = http.get('https://devxhub.com');
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'response time < 2s': (r) => r.timings.duration < 2000,
+  });
+
+  sleep(1); // pause 1 second between requests
+}
